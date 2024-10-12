761526a084f0783609f497ebbd3eb9402bdd7258
tmukhamedjanov <t.mukhamedjanov@mail.ru>
2024-10-12 18:15:07 +0300

    Module 4

---
+++ main_dop.py
@@ -0,0 +1,21 @@
+print("1st program")
+print(9**0.5*5)
+print("")
+
+print("2st program")
+print(9.99 > 9.98 and 1000 != 1000.1)
+print("")
+
+print("3rd program")
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2) == (2*(2+2)))
+print("")
+
+print("4nd program")
+str_ = "123.456"
+str_ = float(str_)
+str_ = int(str_ * 10)
+print(str_)
+print(str_ % 10)
+print("")
